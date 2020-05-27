1ee96ae0aed60b5ab19eba3552cb6ce3e2c876a7
JoshLDowns <josh.lloyd.downs@gmail.com>
2020-05-27 11:41:18 -0400

    dev environment setup

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import path from 'path'
+const app = express();
+const port = process.env.PORT || 5000;
+
+let __dirname = path.resolve(path.dirname(''));
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
+
+app.listen(port, ()=>{console.log(`Listening on port: ${port}`)})
