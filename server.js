76f2000f5a8fa1bd3d0acb4dba0eab473c3714de
Josh Downs <josh.lloyd.downs@gmail.com>
2020-05-27 11:58:39 -0400

    on laptop

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let __dirname = path.resolve(path.dirname(''));
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-  });
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
-app.listen(port, ()=>{console.log(`Listening on port: ${port}`)})
+app.listen(port, () => { console.log(`Listening on port: ${port}`) })
